c9b6f7e143ffa65a31c6e92b1b13adad2e0b0676
Salman Mahmud <salman_mahmud@hotmail.com>
2022-09-22 12:22:55 -0400

    commit tree 521b8a4c4d426fbeedb0ca79f6a9af5a20aab66b

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-function addingEventListener() {
+// function addingEventListener() {
+//     const input = document.getElementById('button')
+//     function clickAlert(){
+//         alert('I was clicked!')
+//     }
+    
+//     input.addEventListener('click', clickAlert)
+// }
+
+
+function addingEventListener(){
+    const input = document.getElementById('button')
+
+    const eventHandler = input.addEventListener('click', (e) =>{ alert('I was clicked')})
+
+    return eventHandler
 }
+
+// const input = document.getElementById('button')
+
+// input.addEventListener('click', (e) =>alert('I was clicked'))
+
+// console.log(input)
+
+addingEventListener()
